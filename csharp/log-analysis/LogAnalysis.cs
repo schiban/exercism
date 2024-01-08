3337c248945551f3a754c1f9780ebcf52827ce80
Schiban <84158141+Schiban@users.noreply.github.com>
2024-01-08 14:09:43 +0000

    Update LogAnalysis.cs

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,16 +1,18 @@
 using System;
+using System.Linq;
+
 
 public static class LogAnalysis 
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
-    public static string SubstringAfter(string log)
-    {
-        return this(log.IndexOf(':') + 2)..;
-    }
+    // TODO: define the 'SubstringAfter()' extension method on the `string` type  
+    public static string SubstringAfter(this string message, string delimiter) => message.Split(delimiter).Last();
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
+    public static string SubstringBetween(this string message, string leftSquareBracket, string rightSquareBracket) =>  message.Split(rightSquareBracket).First().Split(leftSquareBracket).Last();
+
     // TODO: define the 'Message()' extension method on the `string` type
+    public static string Message(this string message) => message.SubstringAfter(": ");
 
     // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string LogLevel(this string message) => message.SubstringBetween("[", "]");
 }
