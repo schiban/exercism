908d979cb3094c1cbe9ae32c5891567a0b79110a
Schiban <84158141+Schiban@users.noreply.github.com>
2024-01-08 18:31:01 +0000

    Update PhoneNumberAnalysis.cs

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -4,7 +4,7 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        return (phoneNumber.Split('-')[0].ToString()=="212", phoneNumber.Split('-')[1].ToString()=="555", phoneNumber.Split('-')[2].ToString());
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
