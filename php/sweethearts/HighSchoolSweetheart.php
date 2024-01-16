42142234caa48776817bc9dd15304aecfb3bac56
João Bandeira <j_bandeira_@hotmail.com>
2024-01-16 08:02:30 +0000

    Completed HighSchoolSweetheart

--- php/sweethearts/HighSchoolSweetheart.php
+++ php/sweethearts/HighSchoolSweetheart.php
@@ -4,27 +4,39 @@ class HighSchoolSweetheart
 {
     public function firstLetter(string $name): string
     {
-        return substr($name, 0);
+        return trim($name)[0];
     }
 
     public function initial(string $name): string
     {
-        return $this->firstLetter($name) . ".";
+        return $this->firstLetter(strtoupper($name)) . ".";
     }
 
     public function initials(string $name): string
     {
         $name = explode(" ", $name);
-        $firstName = $this->initial($name[0]);
-        $lastName = $this->initial($name[1]);
-        return $firstName . " " . $lastName;
-
+        return $this->initial($name[0]) . " " . $this->initial($name[1]);
     }
 
     public function pair(string $sweetheart_a, string $sweetheart_b): string
     {
         $sweetheart_a = $this->initials($sweetheart_a);
         $sweetheart_b = $this->initials($sweetheart_b);
-        return $sweetheart_a . "  +  " . $sweetheart_b;
+        return <<<EXPECTED_HEART
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     $sweetheart_a  +  $sweetheart_b     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    EXPECTED_HEART;
     }
 }
