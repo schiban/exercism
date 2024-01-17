4d035d3c7cfeca9d115127d19a641f65ceee3cba
João Bandeira <j_bandeira_@hotmail.com>
2024-01-17 08:10:35 +0000

    Downloaded and Finished First Task

---
+++ php/windowing-system/ProgramWindow.php
@@ -0,0 +1,17 @@
+<?php
+
+class ProgramWindow
+{
+    public $x;
+    public $y;
+    public $width;
+    public $height;
+
+    function __construct($x, $y, $width, $height)
+    {
+        $this->$x = null;
+        $this->$y = null;
+        $this->$width = null;
+        $this->$height = null;
+    }
+}
