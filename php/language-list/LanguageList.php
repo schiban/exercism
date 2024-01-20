8716a07cb7e4d217ebf0de7db0641e30bf7682a8
João Bandeira <j_bandeira_@hotmail.com>
2024-01-20 00:27:31 +0000

    attemp: task 3 and 4

--- php/language-list/LanguageList.php
+++ php/language-list/LanguageList.php
@@ -9,4 +9,19 @@ function language_list(...$array)
     return $b;
 }
 
-language_list("aaa", "bbbb");
+function add_to_language_list($array, $newLanguage)
+{
+    $array[] = $newLanguage;
+    language_list($array);
+    return $newLanguage;
+}
+
+function prune_language_list($language)
+{
+    return language_list(array_shift($language));
+}
+
+function current_language($element)
+{
+    
+}
